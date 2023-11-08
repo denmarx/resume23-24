e1cff074cf382472b970a8483b50b784101a20c1
undeadgorilla <dennis.marx93@gmail.com>
2023-11-08 22:23:55 +0100

    added basic structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header>
+      <nav className={styles.nav}>hallo</nav>
+    </header>
+  );
+};
+
+export default Header;
