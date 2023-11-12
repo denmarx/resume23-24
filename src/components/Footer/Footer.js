abc0f7182f1666d65b9b4a55e9334a19006802ad
undeadgorilla <dennis.marx93@gmail.com>
2023-11-12 11:08:37 +0100

    footer animations

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,34 @@
+import React, { useEffect, useState } from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleScroll = () => {
+    const viewportHeight = window.innerHeight;
+    const halfViewportHeight = viewportHeight / 2;
+    const fullHeight = document.body.scrollHeight;
+    const fromTop = window.scrollY;
+
+    const isAtBottom = fromTop + viewportHeight >= fullHeight;
+    const isPastHalf = fromTop > halfViewportHeight;
+
+    if (isAtBottom || !isPastHalf) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener when the component unmounts
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className={styles.info}>
+    <div className={`${styles.info} ${!isVisible ? styles.hide : ''}`}>
       <div className={styles.infoLeft}>
         <div className={styles.infoContact}>
           <ul>
