30df790d2d5e0ba57e6686dc80d44e0e673bfc87
undeadgorilla <dennis.marx93@gmail.com>
2023-11-13 14:30:41 +0100

    added titles

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,9 +1,16 @@
-import HeaderImg from '../../assets/dev.png';
 import stylesAnimation from '../utils/animations/animations.module.css';
 import styles from '../Content/Content.module.css';
 
 const Content = () => {
-  return <img src={HeaderImg} className={`${stylesAnimation.imageReveal} ${styles.image}`} alt='HeaderImg' />;
+  return (
+    <div className={styles.titleContainer}>
+      <h1 className={styles.title}>Dennis</h1>
+      <br></br>
+      <h1 className={styles.title}>Marx</h1>
+      <section className={`${styles.image} ${stylesAnimation.imageReveal}`}></section>
+    </div>
+  );
+  // return <img src={HeaderImg} className={`${stylesAnimation.imageReveal} ${styles.image}`} alt='HeaderImg' />;
 };
 
 export default Content;
