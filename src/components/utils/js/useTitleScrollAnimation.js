2efa6417591b90f64331608063dcc3b01320d035
undeadgorilla <dennis.marx93@gmail.com>
2023-11-15 16:52:28 +0100

    eased out title scroll animation

--- src/components/utils/js/useTitleScrollAnimation.js
+++ src/components/utils/js/useTitleScrollAnimation.js
@@ -1,19 +1,48 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 
 const useTitleScrollAnimation = () => {
   const [transformValue, setTransformValue] = useState(0);
+  const requestRef = useRef();
+
+  const easeOutCubic = (x) => {
+    return 1 - Math.pow(1 - x, 3);
+  };
+
+  const animate = useCallback(() => {
+    const currentScrollY = window.scrollY;
+    const maxScroll = 100;
+    const scalingFactor = 0.5;
+    const scrollAmount = Math.min(currentScrollY * scalingFactor, maxScroll);
+
+    const newValue = transformValue + (scrollAmount - transformValue) * easeOutCubic(0.05);
+
+    setTransformValue(newValue);
+
+    if (Math.abs(newValue - transformValue) > 1) {
+      requestRef.current = requestAnimationFrame(animate);
+    } else {
+      setTransformValue(scrollAmount);
+    }
+  }, [transformValue]); // Add any other dependencies of animate here
 
   useEffect(() => {
     const handleScroll = () => {
-      let scrollAmount = window.scrollY;
-      let maxScroll = 250;
-
-      setTransformValue(Math.min(scrollAmount, maxScroll));
+      if (requestRef.current) {
+        cancelAnimationFrame(requestRef.current);
+      }
+      requestRef.current = requestAnimationFrame(animate);
     };
 
     window.addEventListener('scroll', handleScroll);
-  }, []);
 
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      if (requestRef.current) {
+        cancelAnimationFrame(requestRef.current);
+      }
+    };
+  }, [animate]); // animate is now a stable function
+  console.log(transformValue);
   return transformValue;
 };
 
