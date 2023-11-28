5a7ad10740400468c2d31f8a9223f369d8ebd912
undeadgorilla <dennis.marx93@gmail.com>
2023-11-28 10:18:01 +0100

    added tablist highlight bar

--- src/components/Content/Jobs.js
+++ src/components/Content/Jobs.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import JobDetails from './JobDetails'; // Import the new component
+import React, { useState, useEffect } from 'react';
+import JobDetails from './JobDetails';
 import styles from './Jobs.module.css';
 
 const Jobs = () => {
   const [activeJob, setActiveJob] = useState('Fraunhofer-Institute');
+  const [highlightStyle, setHighlightStyle] = useState({});
 
   const jobNames = [
     'Fraunhofer-Institute',
@@ -12,10 +13,22 @@ const Jobs = () => {
     'Bremen Education Service',
   ];
 
+  useEffect(() => {
+    const activeIndex = jobNames.indexOf(activeJob);
+    const translateY = `calc(${activeIndex} * 42px)`;
+
+    setHighlightStyle({
+      transform: `translateY(${translateY})`,
+    });
+  }, [activeJob, jobNames]);
+
   return (
     <section id='jobs' className={styles.jobsWrapper}>
       <h2 className={styles.jobHead}>Where I've Worked</h2>
       <div className={styles.inner}>
+        <div className={styles.tabHighlightBar}>
+          <div className={styles.tabHighlight} style={highlightStyle} />
+        </div>
         <div role='tablist' className={styles.tabList}>
           {jobNames.map((jobName) => (
             <button key={jobName} onClick={() => setActiveJob(jobName)}>
@@ -23,9 +36,10 @@ const Jobs = () => {
             </button>
           ))}
         </div>
+
         <div className={styles.jobPanel}>
           {jobNames.map((jobName) => (
-            <JobDetails key={jobName} jobName={jobName} isActive={activeJob === jobName} />
+            <JobDetails jobName={jobName} isActive={activeJob === jobName} />
           ))}
         </div>
       </div>
